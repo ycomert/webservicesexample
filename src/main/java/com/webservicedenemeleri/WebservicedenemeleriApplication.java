8117d1949b43a97b961605057a0c6a387d0d73b7
c8mert <c8mert@gmail.com>
2023-12-23 21:55:34 +0300

    First commit . .

---
+++ src/main/java/com/webservicedenemeleri/WebservicedenemeleriApplication.java
@@ -0,0 +1,13 @@
+package com.webservicedenemeleri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebservicedenemeleriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebservicedenemeleriApplication.class, args);
+	}
+
+}
